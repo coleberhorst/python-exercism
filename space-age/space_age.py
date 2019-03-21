5ee1b32e75a23985efa0c12d3c0f0b2483589981
Cole Berhorst <coleberhorst@gmail.com>
2019-03-20 20:10:37 -0500

    space age lambda solution from https://exercism.io/profiles/ozan

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,22 +1,17 @@
 class SpaceAge(object):
-    EARTH_YEAR_TO_SECONDS = 31557600
+    ORBITAL_PERIODS = [(k, v * 31557600) for k, v in (
+        ('earth', 1.0),
+        ('mercury', 0.2408467),
+        ('venus', 0.61519726),
+        ('mars', 1.8808158),
+        ('jupiter', 11.862615),
+        ('saturn', 29.447498),
+        ('uranus', 84.016846),
+        ('neptune', 164.79132)
+    )]
 
     def __init__(self, seconds):
         self.seconds = seconds
-
-    def on_mercury(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 0.2408467, 2)
-    def on_venus(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 0.61519726, 2)
-    def on_earth(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 1, 2)
-    def on_mars(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 1.8808158, 2)
-    def on_jupiter(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 11.862615, 2)
-    def on_saturn(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 29.447498, 2)
-    def on_uranus(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 84.016846, 2)
-    def on_neptune(self):
-        return round(self.seconds / self.EARTH_YEAR_TO_SECONDS / 164.79132, 2)
+        for planet, p in self.ORBITAL_PERIODS:
+            calc = lambda p=p: round(seconds / p, 2)
+            setattr(self, 'on_' + planet, calc)
