046a49eeb9343a24e56d1169980cf9e0cb28a5ac
Cole Berhorst <coleberhorst@gmail.com>
2019-06-20 20:26:37 -0500

    book store solution

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
+from operator import itemgetter
 
 BOOK_PRICES = [0, 800, 1520, 2160, 2560, 3000]
 
@@ -7,15 +8,19 @@ def calculate_total(books):
     count = defaultdict(int)
     for b in books:
         count[b] += 1
-    largest_set, price = len(set(books)), 0
+    largest_set, set_results = len(set(books)), [0]*6
+    if not largest_set:
+        return 0
 
-    while sum(count.values()):
-        subtract_counter = largest_set
-        print(largest_set, subtract_counter, count)
-        for b in count:
-            if count[b] > 0 and subtract_counter > 0:
-                subtract_counter -= 1
-                count[b] -= 1
-        price += BOOK_PRICES[largest_set - subtract_counter]
-        print(largest_set, subtract_counter, count, price)
-    return price
+    for i in range(1, largest_set + 1):
+        temp_count = OrderedDict(sorted(count.items(), key=itemgetter(1), reverse=True))
+        price = 0
+        while sum(temp_count.values()):
+            subtract_counter = i
+            for b in temp_count:
+                if temp_count[b] > 0 and subtract_counter > 0:
+                    subtract_counter -= 1
+                    temp_count[b] -= 1
+            price += BOOK_PRICES[i - subtract_counter]
+        set_results[i] = price
+    return min(x for x in set_results if x > 0)
