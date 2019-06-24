e4c5ad54d712a49145f0a2adedd750fb9d08732a
Cole Berhorst <coleberhorst@gmail.com>
2019-06-24 16:16:58 -0500

    changed gcd to only calculate on _init and finished power functions, corrected _mul bug

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,56 +1,43 @@
 from __future__ import division
+from math import gcd
 
 
 class Rational(object):
     def __init__(self, numer, denom):
-        self.numer = numer
-        self.denom = denom
-        self.simplify()
+        common_factor = gcd(int(numer), int(denom))
+        self.numer = numer / common_factor
+        if denom < 1:
+            self.numer *= -1
+        self.denom = abs(denom / common_factor)
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
-
-    def simplify(self):
-        a, b = self.numer, self.denom
-        while b:
-            a, b = b, a % b
-        self.numer, self.denom = self.numer / a, self.denom / a
-        if not self.numer:
-            self.denom = 1
-        if not self.denom:
-            raise ZeroDivisionError()
 
     def __repr__(self):
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        self.numer, self.denom = self.numer * other.denom + self.denom * other.numer, self.denom * other.denom
-        self.simplify()
-        return self
+        return Rational(self.numer * other.denom + self.denom * other.numer, self.denom * other.denom)
 
     def __sub__(self, other):
-        self.numer, self.denom = self.numer * other.denom - self.denom * other.numer, self.denom * other.denom
-        self.simplify()
-        return self
+        return Rational(self.numer * other.denom - self.denom * other.numer, self.denom * other.denom)
 
     def __mul__(self, other):
-        print(self, other)
-        self.numer, self.denom = self.numer * self.denom, other.numer * other.denom
-        self.simplify()
-        print(self)
-        return self
+        return Rational(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
-        self.numer = self.numer * other.denom
-        self.denom = self.denom * other.numer
-        self.simplify()
-        return self
+        return Rational(self.numer * other.denom, self.denom * other.numer)
 
     def __abs__(self):
         return Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
-        pass
+        if power < 0:
+            return Rational(self.denom ** abs(power), self.numer ** abs(power))
+        elif power > 0:
+            return Rational(self.numer ** power, self.denom ** power)
+        else:
+            return Rational(1, 1)
 
     def __rpow__(self, base):
-        pass
+        return (base ** self.numer) ** (1 / self.denom)
